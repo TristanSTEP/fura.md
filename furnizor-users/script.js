d59643cd06f6cca723da318583692c042ee7a7bf
Survivalx <goshaz7c@icloud.com>
2024-03-01 18:43:24 +0200

    pop-up added

---
+++ furnizor-users/script.js
@@ -0,0 +1,7 @@
+document.getElementById('openPopupButton').addEventListener('click', function() {
+    document.getElementById('pop-up').style.display = 'block';
+});
+
+document.getElementById('closePopupButton').addEventListener('click', function() {
+    document.getElementById('pop-up').style.display = 'none';
+});
