7d5af0d1962c041000098e41e40d48661c3f3f9e
George 'SurvivalX' Christmas <goshaz7c@icloud.com>
2024-03-07 00:38:31 +0200

    read -> close to finish (responsive not ready)

--- furnizor-users/script-read.js
+++ furnizor-users/script-read.js
@@ -20,3 +20,23 @@
 // })
 
 // update in read.html end
+
+// CONFIRM DELETE POPUP
+
+const confirmPopup =  document.getElementById('confirm-delete-popup');
+const deleteOpen = document.getElementById('delete');
+const closePopup = document.getElementById('confirm-no');
+
+deleteOpen.onclick = function() {
+    confirmPopup.style.display = 'flex';
+}
+
+closePopup.onclick = function() {
+    confirmPopup.style.display = 'none';
+}
+
+window.onclick = function(event) {
+    if (event.target == confirmPopup) {
+        confirmPopup.style.display = 'none';
+    }
+}
