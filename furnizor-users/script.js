e3bb65ccf06dd35859c643976b51ac4cb4c4c964
MariusUtica <u3ica.mariu7@gmail.com>
2024-03-06 17:27:31 +0200

    provider users not finished

--- furnizor-users/script.js
+++ furnizor-users/script.js
@@ -1,9 +1,25 @@
-document.querySelector('.openPopupButton').addEventListener('click', function() {
-    document.getElementById('pop-up').style.display = 'block';
-});
+// document.getElementById('openPopupButton').addEventListener('click', function() {
+//     document.getElementById('pop-up').style.display = 'block';
+// });
 
-document.getElementById('closePopupButton').addEventListener('click', function() {
-    document.getElementById('pop-up').style.display = 'none';
-});
+// document.getElementById('closePopupButton').addEventListener('click', function() {
+//     document.getElementById('pop-up').style.display = 'none';
+// });
 
+const openBtn = document.querySelectorAll(".openPopupButton")
+const popup = document.getElementById("pop-up")
+const closeBtn = document.getElementById("closePopupButton")
 
+openBtn.forEach(element => {
+    element.addEventListener("click", function() {
+        if (popup.style.display === "none") {
+            popup.style.display = "block"
+        }
+    })
+})
+
+closeBtn.addEventListener("click", function() {
+    if (popup.style.display === "block") {
+        popup.style.display = "none"
+    }
+})
