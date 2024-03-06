7ff2c3537bb6932aa1e3d316a7a6f3dd8cff8da8
Survivalx <goshaz7c@icloud.com>
2024-03-06 19:26:34 +0200

    layout read.html modified

--- furnizor-users/script-read.js
+++ furnizor-users/script-read.js
@@ -1,21 +1,22 @@
 // update in read.html start
 
-let form = document.getElementById("form-read");
-let cancelBtn = document.getElementById("cancel");
-let openBtn = document.getElementById("edit-btn");
+// let form = document.getElementById("form-read");
+// let cancelBtn = document.getElementById("cancel");
+// let openBtn = document.getElementById("edit-btn");
 
-openBtn.addEventListener("click", function() {
-    if (form.style.display === "none") {
-        form.style.display = "block";
-    } else {
-        form.style.display = "none";
-    }
-})
 
-cancelBtn.addEventListener("click", function() {
-    if (form.style.display === "block") {
-        form.style.display = "none";
-    }
-})
+// openBtn.addEventListener("click", function() {
+//     if (form.style.display === "none") {
+//         form.style.display = "block";
+//     } else {
+//         form.style.display = "none";
+//     }
+// })
+
+// cancelBtn.addEventListener("click", function() {
+//     if (form.style.display === "block") {
+//         form.style.display = "none";
+//     }
+// })
 
 // update in read.html end
